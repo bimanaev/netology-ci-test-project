9058bfc9c920ea169375b8297d8cc665c93def60
bimanaev@gmail.com <bimanaev@gmail.com>
2020-06-17 13:58:29 +0300

    TestInstance PER_CLASS added to test class

--- netology-ci/src/test/java/ru/bimanaev/netology/CashbackHackServiceTest.java
+++ netology-ci/src/test/java/ru/bimanaev/netology/CashbackHackServiceTest.java
@@ -1,12 +1,14 @@
 package ru.bimanaev.netology;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CashbackHackServiceTest {
     private CashbackHackService service = new CashbackHackService();
 
